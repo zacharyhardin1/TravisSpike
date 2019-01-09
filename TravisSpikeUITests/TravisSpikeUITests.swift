a7bd821975bbc69212077aad772b9681e4af4feb
Zachary Hardin <zhardin@pillartechnology.com>
2019-01-09 13:08:18 -0600

    Fix tests.

--- TravisSpikeUITests/TravisSpikeUITests.swift
+++ TravisSpikeUITests/TravisSpikeUITests.swift
@@ -8,7 +8,7 @@ class TravisSpikeUITests: XCTestCase {
     }
 
     func testExample() {
-        XCTAssert(XCUIApplication().staticTexts["Hello Worldj"].waitForExistence(timeout: 2.0))
+        XCTAssert(XCUIApplication().staticTexts["Hello World"].waitForExistence(timeout: 2.0))
     }
 
 }
