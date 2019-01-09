c5fb30652468b716b19b3e29e2ced23b8032a377
Zachary Hardin <zhardin@pillartechnology.com>
2019-01-09 10:48:06 -0600

    Initial Commit

---
+++ TravisSpike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisSpike
+//
+//  Created by Zachary Hardin on 1/9/19.
+//  Copyright © 2019 PillarTechnology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
